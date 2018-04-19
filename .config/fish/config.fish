2fdf98d05dcd862aa05af58e9bcfb9f5af9f4db2
Panjie Setiawan Wicaksono <panjie@panjiesw.com>
2018-04-19 22:56:24 +0700

    update before backup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
+# Base16 Shell
+if status --is-interactive
+    eval sh $HOME/.config/base16-shell/scripts/base16-atelier-dune.sh
+end
+
 # Env Variables
 set -x TERMINAL xfce4-terminal
 set -x EDITOR nvim
@@ -8,3 +13,4 @@ alias vim=nvim
 alias conf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias nvconf='vim ~/.config/nvim/init.vim'
 alias myd='vim ~/.i3/config'
+alias fconf='vim ~/.config/fish/config.fish'
